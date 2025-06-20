f1770393052acd890f9d93f14d46024882c42f54
Aleksandr Reshetov <reshalexit@gmail.com>
2025-06-20 23:25:02 +1000

    Незначительные изменения

--- tests/settings.go
+++ tests/settings.go
@@ -4,5 +4,4 @@ var Port = 7540
 var DBFile = "../data/scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJoYXNoIjoiODM0MWE2N2RlNDJlY2Y1NjRlNTc4ZTI3ZTUzOWU5MWNlZmViMzM2ZGE5N2FkOGYzMWQ4ZTE4ODdkODJhYjk3MiJ9.jW5yYhpcx1XxBd-8rhYkXhvMFiFeYLUkj8_xzOl_PSc
-`
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJoYXNoIjoiODM0MWE2N2RlNDJlY2Y1NjRlNTc4ZTI3ZTUzOWU5MWNlZmViMzM2ZGE5N2FkOGYzMWQ4ZTE4ODdkODJhYjk3MiJ9.jW5yYhpcx1XxBd-8rhYkXhvMFiFeYLUkj8_xzOl_PSc`
